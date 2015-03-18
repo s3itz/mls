59a3a566da7f6db3566e0c9ee3bb4a6f92779251
Daniel Seitz <daniel.e.seitz@gmail.com>
2015-03-18 16:00:15 -0500

    Finalize changes to databases

--- mls_scraper/models.py
+++ mls_scraper/models.py
@@ -1,4 +1,5 @@
-from sqlalchemy import Column, Integer, String, Float, ForeignKey
+from sqlalchemy import Column, Integer, String, Float, ForeignKey, DateTime, \
+    Table
 from sqlalchemy.orm import relationship
 from .database import Base
 
@@ -8,11 +9,13 @@ class Conference(Base):
 
     id = Column(Integer, primary_key=True)
     name = Column(String, nullable=False)
-    clubs = relationship('ClubStanding', backref='conference')
+
+    clubs = relationship('ClubStanding',
+                         cascade="all, delete-orphan",
+                         backref='conference',
+                         order_by='ClubStanding.rank')
 
 
-# Consider renaming this to just Standing and associate it with a team
-# possibly dropping the name field too?
 class ClubStanding(Base):
     __tablename__ = 'club_standing'
 
@@ -32,4 +35,45 @@ class ClubStanding(Base):
     home_goals_difference = Column(Integer, nullable=False)
     road_goals = Column(Integer, nullable=False)
     road_goals_difference = Column(Integer, nullable=False)
-    conference_id = Column(Integer, ForeignKey('conference.id'), nullable=False)
+
+    conference_id = Column(Integer,
+                           ForeignKey('conference.id',
+                                      onupdate='CASCADE',
+                                      ondelete='CASCADE'),
+                           nullable=False)
+
+
+scheduled_game_broadcaster = \
+    Table('scheduled_game_broadcaster',
+          Base.metadata,
+          Column('scheduled_game_id',
+                 Integer,
+                 ForeignKey('scheduled_game.id', ondelete='CASCADE')),
+          Column('broadcaster_id',
+                 Integer,
+                 ForeignKey('broadcaster.id',
+                            ondelete='CASCADE'))
+          )
+
+
+class ScheduledGame(Base):
+    __tablename__ = 'scheduled_game'
+
+    id = Column(Integer, primary_key=True)
+    time = Column(DateTime, nullable=False)
+    home_team = Column(String, nullable=False)
+    home_score = Column(Integer, nullable=False, default=0)
+    away_team = Column(String, nullable=False)
+    away_score = Column(Integer, nullable=False, default=0)
+    matchcenter_url = Column(String, nullable=False)
+
+    broadcasters = relationship('Broadcaster',
+                                secondary=scheduled_game_broadcaster,
+                                backref='scheduled_games')
+
+
+class Broadcaster(Base):
+    __tablename__ = 'broadcaster'
+
+    id = Column(Integer, primary_key=True)
+    name = Column(String, nullable=False)
