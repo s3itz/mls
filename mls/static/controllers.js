f7e6b3dc5118f8567040412802c1e5fc366e61b0
Daniel Seitz <daniel.e.seitz@gmail.com>
2015-03-20 04:51:53 -0500

    Add schedule to view (current month, prototype).

--- mls/static/controllers.js
+++ mls/static/controllers.js
@@ -1,16 +1,29 @@
-(function() {
+(function () {
   'use strict';
 
   var app = angular.module('mlsAggregator', []);
 
   app.controller('StandingsCtrl', function ($scope, $http) {
-    $http.get('/standings').
-      success(function (data, status, headers, config) {
-        $scope.standings = data
-        console.log(data)
-      }).
-      error(function (data, status, headers, config) {
+    $http.get('/standings')
+      .success(function (data) {
+        $scope.standings = data;
+      })
+      .error(function (data) {
         console.log('Error:', data)
+      });
+  });
+
+  app.controller('ScheduleCtrl', function ($scope, $http) {
+    var date = new Date();
+    var url = '/schedule/2015/' + (date.getMonth() + 1);
+    console.log(url);
+    $http.get(url)
+      .success(function (data) {
+        $scope.schedule = data;
+        console.log($scope.schedule)
+      })
+      .error(function (data) {
+        console.log(data);
       });
   });
 })();
